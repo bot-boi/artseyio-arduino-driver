d4b09ca8fb5dfaba83959f725b2e28cd449e3027
blu3cereal <nobody@noone.com>
2021-09-11 13:28:41 -0400

    Add global combos
    	* lower time required to trigger HOLD from 1s to 800ms
    	* lower mouse movestep significantly (50-15)
    	* spam keyRelease only when a key is or just was
    	  pressed, as opposed to all the time.  note that this needs to
    	  happen otherwise keys can get "stuck" in the bluetooth sense

--- artsey-arduino-driver.ino
+++ artsey-arduino-driver.ino
@@ -5,18 +5,22 @@
 
 
 /* ============================== user configuration ============================== */
+// REVIEW: these could be macros
 // bool mode_left = true;  // lefthanded or no
 int press_delay = 50;   // wait N ms after the initial press before reading the state of the keys
 int release_delay = 0;  // wait N ms after release
 int poll_delay_ms = 0;  // wait N ms every tick
+int hold_time_ms = 800;    // how long it takes for a held key to be HOLD instead of TAP
 
 // maps pins (the elements) to their corresponding values (the indicies of a keymap)
 int pins[8] = {11, 7, 15, 16, // <--- left handed
                29, 31, 30, 27};
 //int pins[8] = {16, 15, 7, 11,  <--- right handed
 //               27, 30, 31, 29};  
-//char layer_primary_keymap[8] = {'a', 'r', 't', 's',
-//                               'e', 'y', 'i', 'o'};
+// right = {'a', 'r', 't', 's',
+//          'e', 'y', 'i', 'o'};
+// left  = {'s', 't', 'r', 'a',
+//          'o', 'i', 'y', 'e'};
 
 enum PRESS_TYPE {
   TAP,
@@ -61,7 +65,7 @@ int toggle_modifier(Combo combo) {
   return 0;
 }
 
-#define MSTEP 50
+#define MSTEP 15
 int mouse_move_up(Combo combo) {
   blehid.mouseMove(0, -MSTEP);
   return 0;
@@ -96,7 +100,33 @@ int mouse_click_right(Combo combo) {
   return 0;
 }
 
+// global combos, meant to be present in every layer
+#define ENTER           {238, TAP, HID_KEY_ENTER,        NULL,                        send_key},
+#define BACKSPACE       {237, TAP, HID_KEY_BACKSPACE,    NULL,                        send_key},
+#define CTRL_LEFT_ONCE  {231, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTCTRL,  accumulate_modifier},
+#define SHIFT_LEFT_ONCE {225, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTSHIFT, accumulate_modifier},  
+#define SHIFT_LEFT_LOCK {221, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTSHIFT, toggle_modifier},      
+#define GUI_LEFT_ONCE   {215, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTGUI,   accumulate_modifier},  // aka super key?
+#define DELETE          {189, TAP, HID_KEY_DELETE,       NULL,                        send_key},
+#define ALT_LEFT_ONCE   {183, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTALT,   accumulate_modifier},
+#define ESCAPE          {124, TAP, HID_KEY_ESCAPE,       NULL,                        send_key},
+#define TAB             {120, TAP, HID_KEY_TAB,          NULL,                        send_key},
+#define SPACE           {15,  TAP, HID_KEY_SPACE,        NULL,                        send_key},
+#define GLOBAL_COMBOS \
+          ENTER \
+          BACKSPACE \
+          CTRL_LEFT_ONCE \
+          SHIFT_LEFT_ONCE \
+          SHIFT_LEFT_LOCK \
+          GUI_LEFT_ONCE \
+          DELETE \
+          ALT_LEFT_ONCE \
+          ESCAPE \
+          TAB \
+          SPACE
+
 struct Combo layer_numbers[] = {
+  GLOBAL_COMBOS
   {254, TAP, HID_KEY_1,            NULL,                        send_key},
   {253, TAP, HID_KEY_2,            NULL,                        send_key},
   {252, TAP, HID_KEY_7,            NULL,                        send_key},
@@ -104,7 +134,6 @@ struct Combo layer_numbers[] = {
   {249, TAP, HID_KEY_8,            NULL,                        send_key},
   {247, TAP, HID_KEY_NONE,         NULL,                        set_layer_primary},
   {239, TAP, HID_KEY_4,            NULL,                        send_key},
-  {237, TAP, HID_KEY_BACKSPACE,    NULL,                        send_key},
   {223, TAP, HID_KEY_5,            NULL,                        send_key},
   {207, TAP, HID_KEY_9,            NULL,                        send_key},
   {191, TAP, HID_KEY_6,            NULL,                        send_key},
@@ -113,6 +142,7 @@ struct Combo layer_numbers[] = {
 int layer_numbers_len = sizeof layer_numbers / sizeof layer_numbers[0];
 
 struct Combo layer_brackets[] = {
+  GLOBAL_COMBOS
   {254, TAP, HID_KEY_NONE,         NULL,                        set_layer_primary},
   {253, TAP, HID_KEY_0,            KEYBOARD_MODIFIER_LEFTSHIFT, send_key},  // )
   {251, TAP, HID_KEY_9,            KEYBOARD_MODIFIER_LEFTSHIFT, send_key},  // (
@@ -124,6 +154,7 @@ struct Combo layer_brackets[] = {
 int layer_brackets_len = sizeof layer_brackets / sizeof layer_brackets[0];
 
 struct Combo layer_symbols[] = {
+  GLOBAL_COMBOS
   {254, TAP, HID_KEY_1,            KEYBOARD_MODIFIER_LEFTSHIFT, send_key},  // !
   {253, TAP, HID_KEY_BACKSLASH,    NULL,                        send_key},
   {251, TAP, HID_KEY_SEMICOLON,    NULL,                        send_key},
@@ -136,6 +167,7 @@ struct Combo layer_symbols[] = {
 int layer_symbols_len = sizeof layer_symbols / sizeof layer_symbols[0];
 
 struct Combo layer_navigation[] = {
+  GLOBAL_COMBOS
   {254, TAP, HID_KEY_END,          NULL,                        send_key},
   {253, TAP, HID_KEY_ARROW_UP,     NULL,                        send_key},
   {251, TAP, HID_KEY_HOME,         NULL,                        send_key},
@@ -149,6 +181,7 @@ struct Combo layer_navigation[] = {
 int layer_navigation_len = sizeof layer_navigation / sizeof layer_navigation[0];
 
 struct Combo layer_mouse[] = {
+  GLOBAL_COMBOS
   {254, TAP, HID_KEY_NONE,         NULL,                        mouse_click_left},
   {253, TAP, HID_KEY_NONE,         NULL,                        mouse_move_up},
   {251, TAP, HID_KEY_NONE,         NULL,                        mouse_click_right},
@@ -162,6 +195,7 @@ struct Combo layer_mouse[] = {
 int layer_mouse_len = sizeof layer_mouse / sizeof layer_mouse[0];
 
 struct Combo layer_primary[] = {
+  GLOBAL_COMBOS
   {254, TAP, HID_KEY_A,            NULL,			                  send_key},
   {254, HOLD, HID_KEY_A,           NULL,                        set_layer_brackets},
   {253, TAP, HID_KEY_R,            NULL,			                  send_key},
@@ -179,21 +213,13 @@ struct Combo layer_primary[] = {
   {240, TAP, HID_KEY_Z,            NULL,			                  send_key},
   {239, TAP, HID_KEY_E,            NULL,			                  send_key},
   {239, HOLD, HID_KEY_NONE,         NULL,                       set_layer_symbols},
-  {238, TAP, HID_KEY_ENTER,        NULL,			                  send_key},
-  {237, TAP, HID_KEY_BACKSPACE,    NULL,			                  send_key},
-  {231, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTCTRL,	accumulate_modifier},
-  {225, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTSHIFT, accumulate_modifier},
   {223, TAP, HID_KEY_Y,            NULL,			                  send_key},
   {222, TAP, HID_KEY_COMMA,        NULL,			                  send_key},
-  {221, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTSHIFT, toggle_modifier},
   {218, TAP, HID_KEY_NONE,         NULL,                        set_layer_mouse},
-  {215, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTGUI,   accumulate_modifier},
   {207, TAP, HID_KEY_C,            NULL,			                  send_key},
   {191, TAP, HID_KEY_I,            NULL,			                  send_key},
   {190, TAP, HID_KEY_PERIOD,       NULL,			                  send_key},
-  {189, TAP, HID_KEY_DELETE,       NULL,			                  send_key},
   {187, TAP, HID_KEY_1,            KEYBOARD_MODIFIER_LEFTSHIFT, send_key},  // !
-  {183, TAP, HID_KEY_NONE,         KEYBOARD_MODIFIER_LEFTALT,   accumulate_modifier},
   {175, TAP, HID_KEY_H,            NULL,			                  send_key},
   {173, TAP, HID_KEY_NONE,         NULL,                        set_layer_navigation},
   {159, TAP, HID_KEY_U,            NULL,			                  send_key},
@@ -201,15 +227,13 @@ struct Combo layer_primary[] = {
   {143, TAP, HID_KEY_L,            NULL,			                  send_key},
   {127, TAP, HID_KEY_O,            NULL,			                  send_key},
   {126, TAP, HID_KEY_SLASH,        NULL,			                  send_key},
-  {124, TAP, HID_KEY_ESCAPE,       NULL,			                  send_key},
-  {120, TAP, HID_KEY_TAB,          NULL,			                  send_key},
   {111, TAP, HID_KEY_B,            NULL,			                  send_key},
   {95,  TAP, HID_KEY_K,            NULL,			                  send_key},
   {63,  TAP, HID_KEY_N,            NULL,			                  send_key},
   {47,  TAP, HID_KEY_P,            NULL,			                  send_key},
   {31,  TAP, HID_KEY_M,            NULL,			                  send_key},
   {30,  TAP, HID_KEY_CAPS_LOCK,    NULL,			                  send_key},
-  {15,  TAP, HID_KEY_SPACE,        NULL,			                  send_key},
+
 };
 int layer_primary_len = sizeof layer_primary / sizeof layer_primary[0];
 
@@ -323,6 +347,11 @@ bool is_key_pressed = false;
 unsigned long press_start_time;
 uint8_t active_code = 0;
 
+// log of keyboard state, only becomes active when keys are pressed
+#define STATE_LOG_SIZE 100
+int state_log[STATE_LOG_SIZE] = {0};
+int *state_log_ptr = state_log;
+
 
 void tick() {
   uint8_t code_in = get_keyboard_state(pins);
@@ -348,7 +377,7 @@ void tick() {
     if (is_key_pressed == true) {
       unsigned long press_time = millis() - press_start_time; // how long of a keypress
       PRESS_TYPE press_type = TAP;
-      if (press_time >= 1000) { // one second
+      if (press_time >= hold_time_ms) { // one second
         press_type = HOLD;
       }
       press_start_time = 0;
@@ -360,9 +389,8 @@ void tick() {
       is_key_pressed = false;
       delay(release_delay);
     }
+    blehid.keyRelease();
   }
-  // blehid.keyboardReport(HID_KEY_NONE);
-  blehid.keyRelease();
   delay(poll_delay_ms);
 }
 
